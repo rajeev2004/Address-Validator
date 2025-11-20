e70d5fafc6a20a33217ab2020dddc607afda2d91
rajeev <rajeevchoudhary2425@gmail.com>
2025-11-20 12:12:05 +0530

    Added backend live link

--- frontend/src/api/validateAddress.js
+++ frontend/src/api/validateAddress.js
@@ -2,7 +2,7 @@ import axios from "axios";
 
 export const validateAddress = async (payload) => {
   const res = await axios.post(
-    "http://localhost:5000/api/address/validate",
+    "https://address-validator-eaex.onrender.com/api/address/validate",
     payload
   );
   return res.data;
